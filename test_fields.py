982a7a18d1df18cba585886c0d748fb2433b9b49
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-09-19 18:22:24 +0100

    has_birthday fields refactored

--- test_fields.py
+++ test_fields.py
@@ -4,75 +4,75 @@ from api import ADMIN_LOGIN, MethodRequest, OnlineScoreRequest, ClientsInterests
 
 
 class TestMethodRequest:
-    #
-    # @pytest.mark.parametrize("account_name", [(""), (" "),])
-    # def test_account_field_can_be_nulled(self, account_name):
-    #     data = {'account': account_name}
-    #     request = MethodRequest(data)
-    #     assert 'account' not in request.invalid_fields
-    #
-    #
-    # def test_account_field_is_not_required(self):
-    #     data = {}
-    #     request = MethodRequest(data)
-    #     assert 'account' not in request.invalid_fields
-    #
-    # @pytest.mark.parametrize("login_name", [(""), (" "), ])
-    # def test_login_field_can_be_nulled(self, login_name):
-    #     data = {'login': login_name}
-    #     request = MethodRequest(data)
-    #     assert 'login' not in request.invalid_fields
-    #
-    # def test_login_field_is_required(self):
-    #     data = {}
-    #     request = MethodRequest(data)
-    #     assert 'login' in request.invalid_fields
-    #
-    # @pytest.mark.parametrize("token_name", [(""), (" "), ])
-    # def test_token_field_can_be_nulled(self, token_name):
-    #     data = {'token': token_name}
-    #     request = MethodRequest(data)
-    #     assert 'token' not in request.invalid_fields
-    #
-    # def test_token_field_is_required(self):
-    #     data = {}
-    #     request = MethodRequest(data)
-    #     assert 'token' in request.invalid_fields
-    #
-    # @pytest.mark.parametrize("arguments_name", [({})])
-    # def test_arguments_field_can_be_nulled(self, arguments_name):
-    #     data = {'arguments': arguments_name}
-    #     request = MethodRequest(data)
-    #     assert 'arguments' not in request.invalid_fields
-    #
-    # def test_arguments_field_is_required(self):
-    #     data = {}
-    #     request = MethodRequest(data)
-    #     assert 'arguments' in request.invalid_fields
-    #
-    # @pytest.mark.parametrize("method_name", [(""), (" "), ])
-    # def test_method_field_cannot_be_nulled(self, method_name):
-    #     data = {'method': method_name}
-    #     request = MethodRequest(data)
-    #     assert 'method' in request.invalid_fields
-    #
-    # def test_method_field_is_required(self):
-    #     data = {}
-    #     request = MethodRequest(data)
-    #     assert 'method' in request.invalid_fields
 
-    @pytest.mark.parametrize("value", [ADMIN_LOGIN])
-    def test_is_admin_property_success(self, value):
-        data = {'login': value}
-        request = MethodRequest(data)
-        assert request.is_admin is True
-
-    @pytest.mark.parametrize("value", ["Yeldos123"])
-    def test_is_admin_property_failure(self, value):
-        data = {'login': value}
-        request = MethodRequest(data)
-        assert request.is_admin is False
-
+#     @pytest.mark.parametrize("account_name", [(""), (" "),])
+#     def test_account_field_can_be_nulled(self, account_name):
+#         data = {'account': account_name}
+#         request = MethodRequest(data)
+#         assert 'account' not in request.invalid_fields
+#
+#
+#     def test_account_field_is_not_required(self):
+#         data = {}
+#         request = MethodRequest(data)
+#         assert 'account' not in request.invalid_fields
+#
+#     @pytest.mark.parametrize("login_name", [(""), (" "), ])
+#     def test_login_field_can_be_nulled(self, login_name):
+#         data = {'login': login_name}
+#         request = MethodRequest(data)
+#         assert 'login' not in request.invalid_fields
+#
+#     def test_login_field_is_required(self):
+#         data = {}
+#         request = MethodRequest(data)
+#         assert 'login' in request.invalid_fields
+#
+#     @pytest.mark.parametrize("token_name", [(""), (" "), ])
+#     def test_token_field_can_be_nulled(self, token_name):
+#         data = {'token': token_name}
+#         request = MethodRequest(data)
+#         assert 'token' not in request.invalid_fields
+#
+#     def test_token_field_is_required(self):
+#         data = {}
+#         request = MethodRequest(data)
+#         assert 'token' in request.invalid_fields
+#
+#     @pytest.mark.parametrize("arguments_name", [({})])
+#     def test_arguments_field_can_be_nulled(self, arguments_name):
+#         data = {'arguments': arguments_name}
+#         request = MethodRequest(data)
+#         assert 'arguments' not in request.invalid_fields
+#
+#     def test_arguments_field_is_required(self):
+#         data = {}
+#         request = MethodRequest(data)
+#         assert 'arguments' in request.invalid_fields
+#
+#     @pytest.mark.parametrize("method_name", [(""), (" "), ])
+#     def test_method_field_cannot_be_nulled(self, method_name):
+#         data = {'method': method_name}
+#         request = MethodRequest(data)
+#         assert 'method' in request.invalid_fields
+#
+#     def test_method_field_is_required(self):
+#         data = {}
+#         request = MethodRequest(data)
+#         assert 'method' in request.invalid_fields
+#
+#     @pytest.mark.parametrize("value", [ADMIN_LOGIN])
+#     def test_is_admin_property_success(self, value):
+#         data = {'login': value}
+#         request = MethodRequest(data)
+#         assert request.is_admin is True
+#
+#     @pytest.mark.parametrize("value", ["Yeldos123"])
+#     def test_is_admin_property_failure(self, value):
+#         data = {'login': value}
+#         request = MethodRequest(data)
+#         assert request.is_admin is False
+#
 #
 #
 # class TestOnlineScoreRequest:
@@ -131,7 +131,7 @@ class TestMethodRequest:
 #         arguments = {}
 #         request = OnlineScoreRequest(arguments)
 #         assert 'birthday' not in request.invalid_fields
-
+#
 #     @pytest.mark.parametrize("gender", [(0), ])
 #     def test_gender_field_can_be_nulled(self, gender):
 #         arguments = {'gender': gender}
@@ -190,19 +190,19 @@ class TestMethodRequest:
 #         arguments = {'phone': value}
 #         request = OnlineScoreRequest(arguments)
 #         assert request.has_phone is False
-#
-#     @pytest.mark.parametrize("value", ["77059997044", "79069997044"])
-#     def test_has_birthday_field_success(self, value):
-#         arguments = {'birthday': value}
-#         request = OnlineScoreRequest(arguments)
-#         assert request.has_birthday is True
-#
-#     @pytest.mark.parametrize("value", ["", None])
-#     def test_has_birthday_field_failure(self, value):
-#         arguments = {'birthday': value}
-#         request = OnlineScoreRequest(arguments)
-#         assert request.has_birthday is False
-#
+
+    @pytest.mark.parametrize("value", ["16.12.2010", "16.12.1980"])
+    def test_has_birthday_field_success(self, value):
+        arguments = {'birthday': value}
+        request = OnlineScoreRequest(arguments)
+        assert request.has_birthday is True
+
+    @pytest.mark.parametrize("value", ["", None, "1980.10.10", "16-12-1980", "16/12/1980"])
+    def test_has_birthday_field_failure(self, value):
+        arguments = {'birthday': value}
+        request = OnlineScoreRequest(arguments)
+        assert request.has_birthday is False
+
 #     @pytest.mark.parametrize("value", ["77059997044", "79069997044"])
 #     def test_has_gender_field_success(self, value):
 #         arguments = {'gender': value}
